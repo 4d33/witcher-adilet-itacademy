cb3e8d92385f344e6bd881ba16666f88d8662728
4d33.qoo <45070184+4d33@users.noreply.github.com>
2018-11-18 21:58:32 +0600

    Add files via upload

---
+++ Witcher/script.js
@@ -0,0 +1,14 @@
+
+var name = '4d33';
+var surname = 'qoo';
+
+console.log(`Hello ${name} ${surname}`)
+
+var age = 24;
+var future = 12;
+
+console.log(age + future)
+
+var before = 12;
+
+console.log(age - before)
